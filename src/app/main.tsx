3027ad85a34c564ca2304a896e943c9e86816bfb
rsgamertv <Wersuser2@gmail.com>
2024-11-24 22:06:12 +0300

    #maked configuration beautifully ,start making #base-template

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -4,7 +4,6 @@ import { NextUIProvider } from "@nextui-org/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { router } from "./providers/router";
 import { RouterProvider } from "react-router-dom";
-import { ThemeProvider } from "next-themes";
 import { ToastProvider } from "./providers/toast/ToastProvider";
 import "../shared/config/i18n";
 import { Suspense } from "react";
